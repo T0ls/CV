0736c50717856f9cdcc4079018e0164c5d2857a3
Sl0t <giugiust2002@gmail.com>
2024-04-17 23:36:09 +0200

    statrting to create the dropdown menù

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-//import data from './gitHubREST.js';
+import data from './gitHubREST.js';
 
-function hideBlock(x) {
+export function hideBlock(x) {
 	var block = document.getElementById("gitHubProfile");
 	block.style.display = "none";
 	showBlock(x);
 }
 
-function showBlock(x) {
+export function showBlock(x) {
 	var block = document.getElementById("gitHubRepoItem");
 	block.style.display = "block";
 	console.log(x);
-	//console.log(data[x]);
-}	  
+	console.log(data[x]);
+}	 
 
